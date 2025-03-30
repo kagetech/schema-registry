ae01c423eff0a721661e1f7bffd534a8c8a387c2
Dariusz Szpakowski <dariusz.szpakowski@kage.tech>
2025-03-30 23:28:32 +0200

    Implement latest schema retrieval by subject use case (#1)

---
+++ tech.kage.schemaregistry/src/main/java/tech/kage/schemaregistry/entity/SchemaRepository.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2025, Dariusz Szpakowski
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package tech.kage.schemaregistry.entity;
+
+import io.confluent.kafka.schemaregistry.client.rest.entities.Schema;
+import reactor.core.publisher.Flux;
+
+/**
+ * Repository interface for accessing schema data by subject.
+ * 
+ * @author Dariusz Szpakowski
+ */
+public interface SchemaRepository {
+    /**
+     * Retrieves all schemas for the given subject, ordered by version in descending
+     * order.
+     *
+     * @param subject the subject for which to retrieve schemas
+     * 
+     * @return a Flux of schemas, ordered from highest to lowest version, or an
+     *         empty Flux if none are found
+     */
+    Flux<Schema> findBySubjectOrderedByVersionDesc(String subject);
+}
